f1430277778a8d66a34c55de3a13d6b813887be7
mattheau2950 <mmclean@unbc.ca>
2017-03-06 23:25:07 -0800

    K-means 1 itteration works! :)

--- classify.py
+++ classify.py
@@ -3,12 +3,13 @@ from numbapro import cuda
 
 import gdal
 from gdalconst import *
-import PyQt4.QtGui as QtGui
+from PyQt4.QtGui import *
 import PyQt4.uic as uic
 import numpy as np
 import k_means
 import time
-
+import random
+import os
 
 class Sensor:
     LANDSAT_8 = 1
@@ -37,10 +38,10 @@ original_image = None
 platform = None
 
 
-class MyApp(QtGui.QMainWindow, Ui_MainWindow):
+class MyApp(QMainWindow, Ui_MainWindow):
 
     def __init__(self):
-        QtGui.QMainWindow.__init__(self)
+        QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
         self.open_image_button.clicked.connect(self.openfile)
@@ -63,37 +64,38 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
         global x_size
         global y_size
         global filePath
-        filePath = QtGui.QFileDialog.getOpenFileName()
+        filePath = QFileDialog.getOpenFileName()
         if  filePath[-28:-25] == 'LC8':
             platform = Sensor.LANDSAT_8
             data_set = gdal.Open(str(filePath[:-6] + 'B1.TIF'), GA_ReadOnly)
-            band1 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.int16)
+            band1 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
             data_set = gdal.Open(str(filePath[:-6] + 'B2.TIF'), GA_ReadOnly)
-            band2 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.int16)
+            band2 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
             data_set = gdal.Open(str(filePath[:-6] + 'B3.TIF'), GA_ReadOnly)
-            band3 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.int16)
+            band3 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
             data_set = gdal.Open(str(filePath[:-6] + 'B4.TIF'), GA_ReadOnly)
-            band4 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.int16)
+            band4 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
             data_set = gdal.Open(str(filePath[:-6] + 'B5.TIF'), GA_ReadOnly)
-            band5 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.int16)
+            band5 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
             data_set = gdal.Open(str(filePath[:-6] + 'B6.TIF'), GA_ReadOnly)
-            band6 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.int16)
+            band6 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
             data_set = gdal.Open(str(filePath[:-6] + 'B7.TIF'), GA_ReadOnly)
-            band7 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.int16)
+            band7 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
             data_set = gdal.Open(str(filePath[:-6] + 'B9.TIF'), GA_ReadOnly)
-            band9 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.int16)
+            band9 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
             data_set = gdal.Open(str(filePath[:-6] + 'B10.TIF'), GA_ReadOnly)
-            band10 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.int16)
+            band10 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
             data_set = gdal.Open(str(filePath[:-6] + 'B11.TIF'), GA_ReadOnly)
-            band11 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.int16)
+            band11 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
+            print band1[2000, 2500]
         else:
             print 'only LC8 currently supported!'
         self.file_path.setText(filePath)
 
 
 
-        x_size = int(data_set.RasterXSize)
-        y_size = int(data_set.RasterYSize)
+        x_size = int(data_set.RasterYSize)
+        y_size = int(data_set.RasterXSize)
 
     def k_means(self):
         global dev_data
@@ -102,20 +104,43 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
         global preGPU
         global x_size
         global y_size
-        result = np.ndarray([y_size, x_size], dtype=np.int64)
-        if len(preGPU.shape) == 3:
-            dev_result = cuda.device_array([y_size, x_size], dtype=np.int64, stream=stream)
-        else:
-            dev_result = cuda.device_array_like(preGPU[:, :], stream=stream)
+        num_clust = 5
+        max_iteration = 20
+        clusters = np.sort(np.ndarray([num_clust+1, preGPU.shape[2]], dtype=np.uint16)) #Nuber of clusters by number of bands
+        for i in range(1, num_clust + 1):
+            print i
+            x = random.randint(0, x_size - 1)
+            y = random.randint(0, y_size - 1)
+            clusters[i, :] = preGPU[x, y, :]
+            while clusters[i, 0] == 0:
+                x = random.randint(0, x_size-1)
+                y = random.randint(0, y_size-1)
+                clusters[i, :] = preGPU[x, y, :]
+
+        print clusters
+        dev_clusters = cuda.to_device(clusters, stream=stream)
+        result = np.ndarray([x_size, y_size], dtype=np.uint8)
+        dev_result = cuda.device_array([x_size, y_size], dtype=np.uint8, stream=stream)
+
+        dev_data = cuda.to_device(preGPU, stream=stream)
         stream.synchronize()
-        k_means.k_means_classify[1, 1024, stream](dev_data, dev_result, y_size, 5, 20)
+        k_means.k_means_classify[(x_size*y_size)/1024+1, 1024, stream](dev_data, dev_result, x_size, dev_clusters, num_clust, preGPU.shape[2], max_iteration)
         stream.synchronize()
         start = time.time()
         dev_result.copy_to_host(result, stream=stream)
         stream.synchronize()
         print "Transfer result to host in " + str(time.time() - start) + " Seconds"
-        for i in range(1300):
-            print result[i, 0]
+        # for i in range(3000, 3010):
+        #     for j in range(3000, 3010):
+        #         print result[i, j]
+        test = np.empty([3, 3])
+        pixmap = QImage(result, 8011, 4000, QImage.Format_Indexed8)
+        pixmap.setColor(0, qRgb(255, 163, 39))
+        pixmap.setColor(1, qRgb(57, 255, 139))
+        pixmap.setColor(2, qRgb(57, 16, 13))
+        pixmap.setColor(3, qRgb(252, 216, 213))
+        pixmap.setColor(4, qRgb(152, 116, 113))
+        self.preview.setPixmap(QPixmap.fromImage(pixmap))
 
     def loadGPU(self):
         global band1
@@ -135,7 +160,7 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
         preGPU = None
         if self.select_B1.isChecked():
             if preGPU is None:
-                preGPU = np.ascontiguousarray(band1[:, :, np.newaxis], dtype=int)
+                preGPU = np.ascontiguousarray(band1[:, :, np.newaxis], dtype=np.uint16)
             else:
                 preGPU = np.dstack((preGPU, band1))
         if self.select_B2.isChecked():
@@ -198,7 +223,7 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
 
 
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     window = MyApp()
     window.show()
     print "Program Initialized"
