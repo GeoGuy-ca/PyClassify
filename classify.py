b931d9232b99bc7b038fc5894e4da5badb77f971
Matt McLean <mmclean@unbc.ca>
2017-03-07 10:51:43 -0800

    Fix preview

--- classify.py
+++ classify.py
@@ -48,6 +48,55 @@ class MyApp(QMainWindow, Ui_MainWindow):
         self.load_gpu_button.clicked.connect(self.loadGPU)
         self.k_means_button.clicked.connect(self.k_means)
 
+    def openfilestart(self):
+        global band1
+        global band2
+        global band3
+        global band4
+        global band5
+        global band6
+        global band7
+        global band8
+        global band9
+        global band10
+        global band11
+        global platform
+        global x_size
+        global y_size
+        global filePath
+        filePath = '/home/mmclean/LC80490222016230LGN00/LC80490222016230LGN00_B1.TIF'
+        if filePath[-28:-25] == 'LC8':
+            platform = Sensor.LANDSAT_8
+            data_set = gdal.Open(str(filePath[:-6] + 'B1.TIF'), GA_ReadOnly)
+            band1 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
+            data_set = gdal.Open(str(filePath[:-6] + 'B2.TIF'), GA_ReadOnly)
+            band2 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
+            data_set = gdal.Open(str(filePath[:-6] + 'B3.TIF'), GA_ReadOnly)
+            band3 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
+            data_set = gdal.Open(str(filePath[:-6] + 'B4.TIF'), GA_ReadOnly)
+            band4 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
+            data_set = gdal.Open(str(filePath[:-6] + 'B5.TIF'), GA_ReadOnly)
+            band5 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
+            data_set = gdal.Open(str(filePath[:-6] + 'B6.TIF'), GA_ReadOnly)
+            band6 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
+            data_set = gdal.Open(str(filePath[:-6] + 'B7.TIF'), GA_ReadOnly)
+            band7 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
+            data_set = gdal.Open(str(filePath[:-6] + 'B9.TIF'), GA_ReadOnly)
+            band9 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
+            data_set = gdal.Open(str(filePath[:-6] + 'B10.TIF'), GA_ReadOnly)
+            band10 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
+            data_set = gdal.Open(str(filePath[:-6] + 'B11.TIF'), GA_ReadOnly)
+            band11 = np.array(data_set.GetRasterBand(1).ReadAsArray(), dtype=np.uint16)
+            print band1[2000, 2500]
+        else:
+            print 'only LC8 currently supported!'
+        self.file_path.setText(filePath)
+
+
+
+        y_size = int(data_set.RasterYSize)
+        x_size = int(data_set.RasterXSize)
+
     def openfile(self):
         global band1
         global band2
@@ -94,8 +143,8 @@ class MyApp(QMainWindow, Ui_MainWindow):
 
 
 
-        x_size = int(data_set.RasterYSize)
-        y_size = int(data_set.RasterXSize)
+        y_size = int(data_set.RasterYSize)
+        x_size = int(data_set.RasterXSize)
 
     def k_means(self):
         global dev_data
@@ -106,9 +155,10 @@ class MyApp(QMainWindow, Ui_MainWindow):
         global y_size
         num_clust = 5
         max_iteration = 20
+        print x_size
+        print y_size
         clusters = np.sort(np.ndarray([num_clust+1, preGPU.shape[2]], dtype=np.uint16)) #Nuber of clusters by number of bands
         for i in range(1, num_clust + 1):
-            print i
             x = random.randint(0, x_size - 1)
             y = random.randint(0, y_size - 1)
             clusters[i, :] = preGPU[x, y, :]
@@ -117,15 +167,16 @@ class MyApp(QMainWindow, Ui_MainWindow):
                 y = random.randint(0, y_size-1)
                 clusters[i, :] = preGPU[x, y, :]
 
-        print clusters
         dev_clusters = cuda.to_device(clusters, stream=stream)
         result = np.ndarray([x_size, y_size], dtype=np.uint8)
         dev_result = cuda.device_array([x_size, y_size], dtype=np.uint8, stream=stream)
 
         dev_data = cuda.to_device(preGPU, stream=stream)
         stream.synchronize()
-        k_means.k_means_classify[(x_size*y_size)/1024+1, 1024, stream](dev_data, dev_result, x_size, dev_clusters, num_clust, preGPU.shape[2], max_iteration)
+        start = time.time()
+        k_means.k_means_classify[(x_size*y_size)/1024+1, 1024, stream](dev_data, dev_result, y_size, dev_clusters, num_clust, preGPU.shape[2], max_iteration)
         stream.synchronize()
+        print "K-Means: " + str(time.time() - start) + " Seconds"
         start = time.time()
         dev_result.copy_to_host(result, stream=stream)
         stream.synchronize()
@@ -134,13 +185,18 @@ class MyApp(QMainWindow, Ui_MainWindow):
         #     for j in range(3000, 3010):
         #         print result[i, j]
         test = np.empty([3, 3])
-        pixmap = QImage(result, 8011, 4000, QImage.Format_Indexed8)
-        pixmap.setColor(0, qRgb(255, 163, 39))
+        try:
+            pixmap.fill(3)
+        except:
+            print "Pixmap not Def"
+        pixmap = QImage(result, x_size, y_size, y_size, QImage.Format_Indexed8)
+        pixmap.setColor(0, qRgb(0, 0, 0))
         pixmap.setColor(1, qRgb(57, 255, 139))
         pixmap.setColor(2, qRgb(57, 16, 13))
         pixmap.setColor(3, qRgb(252, 216, 213))
         pixmap.setColor(4, qRgb(152, 116, 113))
         self.preview.setPixmap(QPixmap.fromImage(pixmap))
+
 
     def loadGPU(self):
         global band1
@@ -218,6 +274,8 @@ class MyApp(QMainWindow, Ui_MainWindow):
             start = time.time()
             dev_data = cuda.to_device(preGPU, stream=stream)
             print "Transfer to GPU in " + str(time.time() - start) + " Seconds"
+            # pixmap = QImage(preGPU, y_size, x_size, QImage.Format_RGB32)
+            # self.preview.setPixmap(QPixmap.fromImage(pixmap))
         else:
             print "Failed to load GPU"
 
@@ -226,6 +284,9 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyApp()
     window.show()
+    window.openfilestart()
+    window.loadGPU()
+    window.k_means()
     print "Program Initialized"
     sys.exit(app.exec_())
 
--- k_means.py
+++ k_means.py
@@ -5,14 +5,22 @@ import math
 @cuda.jit('void(uint16[:,:,:], uint8[:, :], int16, uint16[:, :], int16, int16, int16)', target='gpu')
 def k_means_classify(image, dev_result, width, clusters, num_clusts, bands, max_iteration):
     pix = cuda.grid(1)
-    x = pix % width
-    y = pix / width
+    y = pix % width
+    x = pix / width
     if image[x, y, 0] != 0:
-        min_dist = 1000000
+        dist = 0
+        for band in range(bands):
+            dist += (image[x, y, band] - clusters[1, band]) ** 2
+        min_dist = dist
+        dev_result[x, y] = 1
         for cluster in range(1, num_clusts + 1):
             dist = 0
             for band in range(bands):
                 dist += (image[x, y, band] - clusters[cluster, band]) ** 2
             if dist < min_dist:
-                dev_result[x, y] = cluster
                 min_dist = dist
+                dev_result[x, y] = cluster
+
+        # dev_result[x, y] = 1
+    else:
+        dev_result[x, y] = 0
